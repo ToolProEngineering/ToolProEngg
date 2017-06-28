557ffa76c58c541c0d60ce1227aeb83d5d9be1bb
Neville <nevil_dabre@yahoo.com>
2017-06-27 22:27:24 -0400

    products

--- public/js/all.js
+++ public/js/all.js
@@ -14,79 +14,75 @@ function cancelEmployers() {
     window.location.href = '/employers';
 }
 
-$( document ).ready(function() {
-    console.log( "ready!" );
+
+$(document).ready(function() {
+
+    
+    
+    function close_accordion_section() {
+        $('.accordion .accordion-section-title').removeClass('active');
+        $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+    }
+ 
+    $('.accordion-section-title').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+ 
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+ 
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+        }
+ 
+        e.preventDefault();
+    });
 });
 
 $(function () {
+
+        $("#accordion").show().accordion({
+        active: false,
+        collapsible: true
+    });
+
+    var $myGroup = $('#accordion');
+       $('#accordion h4').click(function() {
+           console.log('here');
+    $myGroup.find('.collapse.in').collapse('hide');
+});
+
+    
     //to load all the images from the folder
 
-    var parentFolder = "/../images/products/";
-
-    var fileExtension = /\.(jpe?g|png|gif)$/;
-
-    //var productFolders = ["RUBBER_MOULDED_PARTS", "SS_FASTENERS", "SS_PIPE_FITTINGS"];
-    var productFolders = ["SS_FASTENERS"];
-
-    if ($('#product_carousel').length == 1) {
-
-        console.log('index page');
-        var k = 0;
-
-        productFolders.forEach(function (productFolder) {
-            var currentFolder = parentFolder + productFolder;
-
-            var count = 0;
-            var thumbnailbody;
-            var thumbnailHeader = "<div class='item'>" +
-                "<div class='row'>";
-
-            var thumbnailFooter = "</div>" +
-                "</div>";
-
-            $.get(currentFolder).then(function (data) {
-
-
-                $(data).find("a").attr("href", function (i, val) {
-                    //console.log(i);
-
-
-                    if (val.match(fileExtension)) {
-                        thumbnailbody += "<div class='col-md-3'><a href='#' class='thumbnail'><img src='" + val + "' alt='Image'></a></div>";
-
-                        //if (count < 4) {
-                        //    count++;
-                        //} else {
-                            $('#product_carousel > .carousel-inner').delay( 10000 ).append("<div class='item'><div class='row'> "+thumbnailbody+" </div></div>");
-                        //    console.log('here');
-                        //    thumbnailbody = null;
-                        //    k++;
-                        //    count = 0;
-                        //}
-                    }
-
-                });
-
-            });
-
-
-        });
-
-    }
+    //var productFolders = ["SS_FASTENERS"];
 
     if ($('#collapseOne').length == 1) {
 
-
+        $('#collapseOne .row').html('');
+        $('#collapseTwo .row').html('');
+        $('#collapseThree .row').html('');
         //prepare the default insert for the thumbnail div
+        var parentFolder = "/../images/products/";
+
+        var fileExtension = /\.(jpe?g|png|gif)$/;
+
+        var productFolders = ["RUBBER_MOULDED_PARTS", "SS_FASTENERS", "SS_PIPE_FITTINGS"];
 
         productFolders.forEach(function (productFolder) {
             var currentFolder = parentFolder + productFolder;
+            
 
             $.get(currentFolder).then(function (data) {
 
                 $(data).find("a").attr("href", function (i, val) {
+                    
                     if (val.match(fileExtension)) {
-                        var thumbnailBody = "<div class='col-md-4'> " +
+                        var thumbnailBody = "<div class='col-md-3'> " +
                             "<div class='thumbnail'>" +
                             "<div class='products-image'>" +
                             "<img id ='" + productFolder + '-' + i + "' class='products-image' src='" + val + "'>" +
@@ -100,9 +96,9 @@ $(function () {
                         if (productFolder == "SS_FASTENERS") {
                             $('#collapseOne .row').append(thumbnailBody);
                         } else if (productFolder == "SS_PIPE_FITTINGS") {
-                            $('#collapseThree .row').append(thumbnailBody);
+                            $('#collapseTwo .row').append(thumbnailBody);
                         } else if (productFolder == "RUBBER_MOULDED_PARTS") {
-                            $('#collapseFour .row').append(thumbnailBody);
+                            $('#collapseThree .row').append(thumbnailBody);
                         }
 
                         //$("body").append("<img src='" + val + "'>"); //we need just href from data
@@ -111,44 +107,6 @@ $(function () {
             });
         });
     }
-
-
-
-    /* $.ajax({
-         url: "/../images/",
-         success: function (data) {
-             $(data).find("a:contains(.png)").each(function () {
-                 // will loop through 
-                 var images = $(this).attr("href");
- 
-                 $('<p></p>').html(images).appendTo('#collapseOne');
- 
-             });
-         }
-     }); */
-
-    /*  
-  
-  
-  
-  
-          $.ajax({
-              url: currentFolder,
-              success: function (data) {
-                  $.each(data, function (i, val) {
-                      var htmlContent = "<div class='thumbnail'><div class='products-image'> <a href='#'> <img class='products-image' src='"+ currentFolder + val +"' alt='61'>" +
-                                          + "</div>  <div class='caption'> <p class'text-center'> Product 1 Description </p> </div> </a> </div> </div>"
-  
-                      console.log(i);
-                      if (val.match(/\.(jpe?g|png|gif)$/)) {
-                          $("#collapseOne .row").append("");
-  
-                      } 
-                  });
-              }
-          });
-  
-      }); */
 
 });
 
